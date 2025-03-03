2ef469fea2956e6f20ccd6327b7264e8ad3ef963
Daniil <bondarev@surf.dev>
2025-03-03 06:26:21 +0300

    feat: Add post grid

--- lib/main.dart
+++ lib/main.dart
@@ -9,10 +9,42 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
+        appBar: AppBar(
+          automaticallyImplyLeading: false,
+          centerTitle: true,
+          title: const Text('Постограм'),
+          actions: [
+            GestureDetector(
+              onTap: () {},
+              child:
+                  const Padding(padding: EdgeInsets.only(right: 20), child: Icon(Icons.more_vert)),
+            ),
+          ],
+        ),
+        body: Padding(
+          padding: const EdgeInsets.symmetric(
+            horizontal: 12,
+            vertical: 16,
+          ),
+          child: GridView.builder(
+            gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+              crossAxisCount: 3,
+            ),
+            itemCount: 10,
+            itemBuilder: (_, index) => SizedBox.square(
+              child: Padding(
+                padding: const EdgeInsets.all(4),
+                child: Image.network(
+                  'https://preview.redd.it/does-anyone-other-than-me-know-where-this-cat-is-from-lol-v0-img24gu05lfb1.jpg?width=1080&crop=smart&auto=webp&s=c3f0a1639fc3a21e04864423c108302d5a36ff8b',
+                  width: double.infinity,
+                  height: double.infinity,
+                  fit: BoxFit.cover,
+                ),
+              ),
+            ),
+          ),
         ),
       ),
     );
