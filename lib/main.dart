8cc0d4b17648d6a16ef1fd21f9dd6f25b5514d70
Daniil <bondarev@surf.dev>
2025-03-03 06:37:19 +0300

    feat: Add load button

--- lib/main.dart
+++ lib/main.dart
@@ -4,8 +4,15 @@ void main() {
   runApp(const MainApp());
 }
 
-class MainApp extends StatelessWidget {
+class MainApp extends StatefulWidget {
   const MainApp({super.key});
+
+  @override
+  State<MainApp> createState() => _MainAppState();
+}
+
+class _MainAppState extends State<MainApp> {
+  int itemCount = 10;
 
   @override
   Widget build(BuildContext context) {
@@ -28,22 +35,33 @@ class MainApp extends StatelessWidget {
             horizontal: 12,
             vertical: 16,
           ),
-          child: GridView.builder(
-            gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-              crossAxisCount: 3,
-            ),
-            itemCount: 10,
-            itemBuilder: (_, index) => SizedBox.square(
-              child: Padding(
-                padding: const EdgeInsets.all(4),
-                child: Image.network(
-                  'https://preview.redd.it/does-anyone-other-than-me-know-where-this-cat-is-from-lol-v0-img24gu05lfb1.jpg?width=1080&crop=smart&auto=webp&s=c3f0a1639fc3a21e04864423c108302d5a36ff8b',
-                  width: double.infinity,
-                  height: double.infinity,
-                  fit: BoxFit.cover,
+          child: Column(
+            children: [
+              Expanded(
+                child: GridView.builder(
+                  gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                    crossAxisCount: 3,
+                  ),
+                  itemCount: itemCount,
+                  itemBuilder: (_, index) => SizedBox.square(
+                    child: Padding(
+                      padding: const EdgeInsets.all(4),
+                      child: Image.network(
+                        'https://preview.redd.it/does-anyone-other-than-me-know-where-this-cat-is-from-lol-v0-img24gu05lfb1.jpg?width=1080&crop=smart&auto=webp&s=c3f0a1639fc3a21e04864423c108302d5a36ff8b',
+                        width: double.infinity,
+                        height: double.infinity,
+                        fit: BoxFit.cover,
+                      ),
+                    ),
+                  ),
                 ),
               ),
-            ),
+              ElevatedButton(
+                  onPressed: () => setState(() {
+                        itemCount += 10;
+                      }),
+                  child: const Text('Загрузить ещё')),
+            ],
           ),
         ),
       ),
